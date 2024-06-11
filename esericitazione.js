4cb53ed24ef97fc9d5173c056d525d89d3baf243
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-06-11 23:34:55 +0200

    aggiunta funzione al button delete

--- esericitazione.js
+++ esericitazione.js
@@ -7,19 +7,15 @@ class Utente {
   }
 }
 /* genero una lista che stampi i nomi in una lista */
-const generaLista = (utenti) => {
-  const listaNomi = document.getElementById("lista-nomi");
-  const lista = document.createElement("ul");
-  lista.style.listStyle = "none";
-  lista.style.marginBlock = "10px";
-  const elementiLista = document.createElement("li");
-  elementiLista.innerHTML = `${utenti.user}`;
+const generaLista = (utente) => {
+  const listaNomi = document.querySelector("#lista-nomi > ul");
 
-  listaNomi.appendChild(lista);
-  lista.appendChild(elementiLista);
+  const elementoLista = document.createElement("li");
+  elementoLista.classList.add("list-group-item");
+  elementoLista.innerText = utente.user;
+
+  listaNomi.appendChild(elementoLista);
 };
-
-/* funzione cancella */
 
 /* genera allert */
 const generaAllert = () => {
@@ -50,13 +46,20 @@ form.onsubmit = (event) => {
 
   form.reset();
 };
+/* elimina nome dalla lista */
+const removeName = () => {
+  const nomiSalvati = JSON.parse(localStorage.getItem("lista-memory"));
+  nomiSalvati.pop();
+  users = nomiSalvati;
+  localStorage.setItem("lista-memory", JSON.stringify(nomiSalvati));
+  const listaNomi = document.querySelector("#lista-nomi > ul");
+  listaNomi.innerHTML = "";
+  nomiSalvati.forEach((obj) => generaLista(obj));
+};
 
 const btnDelete = document.getElementById("btn-delete");
 btnDelete.addEventListener("click", (event) => {
-  event.preventDefault();
-  const nomiSalvati = JSON.parse(localStorage.getItem("lista-memory"));
-  nomiSalvati.pop();
-  console.log(nomiSalvati);
+  removeName();
 });
 
 window.addEventListener("DOMContentLoaded", () => {
